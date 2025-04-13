48eb18444848e7d2c7ebf219e3c3fe58fc066788
Ahmed Mohamed <albashengineerahmed@gmail.com>
2025-04-13 07:25:04 +0200

    update api url

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const baseUrl = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://backend-ecommerce-test.vercel.app",
 });
 
 export default baseUrl
